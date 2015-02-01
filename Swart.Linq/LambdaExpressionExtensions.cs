fcb827cde334a502125c5fca35ab52e9f5215980
moahelmy <muhammad.cs@gmail.com>
2015-02-01 15:19:02 +1100

    Add existing code

---
+++ Swart.Linq/LambdaExpressionExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Swart.DomainDrivenDesign.Linq
+{
+    public static class LambdaExpressionExtensions
+    {
+        public static string GetMemberName(this LambdaExpression memberSelector)
+        {
+            var currentExpression = memberSelector.Body;
+
+            while (true)
+            {
+                switch (currentExpression.NodeType)
+                {
+                    case ExpressionType.Parameter:
+                        return ((ParameterExpression)currentExpression).Name;
+                    case ExpressionType.MemberAccess:
+                        return ((MemberExpression)currentExpression).Member.Name;
+                    case ExpressionType.Call:
+                        return ((MethodCallExpression)currentExpression).Method.Name;
+                    case ExpressionType.Convert:
+                    case ExpressionType.ConvertChecked:
+                        currentExpression = ((UnaryExpression)currentExpression).Operand;
+                        break;
+                    case ExpressionType.Invoke:
+                        currentExpression = ((InvocationExpression)currentExpression).Expression;
+                        break;
+                    case ExpressionType.ArrayLength:
+                        return "Length";
+                    default:
+                        throw new Exception("not a proper member selector");
+                }
+            }
+        }        
+         
+        public static string GetMemberNameForPropertiesOrFieldsOnly(this LambdaExpression memberSelector)
+        {
+            var currentExpression = memberSelector.Body;
+
+            while (true)
+            {
+                switch (currentExpression.NodeType)
+                {
+                    case ExpressionType.Parameter:
+                        return ((ParameterExpression)currentExpression).Name;
+                    case ExpressionType.MemberAccess:
+                        return ((MemberExpression)currentExpression).Member.Name;
+                    case ExpressionType.Call:
+                        throw new ArgumentException("Not property or member field");
+                    case ExpressionType.Convert:
+                    case ExpressionType.ConvertChecked:
+                        currentExpression = ((UnaryExpression)currentExpression).Operand;
+                        break;
+                    case ExpressionType.Invoke:
+                        currentExpression = ((InvocationExpression)currentExpression).Expression;
+                        break;
+                    case ExpressionType.ArrayLength:
+                        return "Length";
+                    default:
+                        throw new ArgumentException("Not a proper member selector");
+                }
+            }
+        }
+    }
+}
---
+++ Swart.Linq/ObjectExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Swart.DomainDrivenDesign.Linq
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Get all properties that are objects of derived classes of the given type.
+        /// </summary>
+        /// <param name="obj">object</param>
+        /// <param name="type">base type</param>
+        /// <returns></returns>
+        public static IEnumerable<PropertyInfo> GetAllPropertiesOfType(this object obj, Type type)
+        {
+            return obj.GetType().GetAllPropertiesOfType(type);
+        }
+
+        /// <summary>
+        /// Get all properties that are objects of derived classes of the given type.
+        /// </summary>
+        /// <param name="type">type</param>
+        /// <param name="testedType">tested type</param>
+        /// <returns></returns>
+        public static IEnumerable<PropertyInfo> GetAllPropertiesOfType(this Type type, Type testedType)
+        {
+            return type.GetProperties().Where(p => p.PropertyType.IsSubclassOf(testedType));
+        }
+    }
+}
